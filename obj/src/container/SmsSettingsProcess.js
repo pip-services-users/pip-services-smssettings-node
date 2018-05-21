d007fc400d737baf42fc2a35991983d47e122698
pipdeveloper <pipdevs@gmail.com>
2018-05-21 12:59:23 +0000

    project build by Travis CI [skip ci]

--- obj/src/container/SmsSettingsProcess.js
+++ obj/src/container/SmsSettingsProcess.js
@@ -1,11 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const pip_services_container_node_1 = require("pip-services-container-node");
+const pip_services_net_node_1 = require("pip-services-net-node");
+const pip_services_oss_node_1 = require("pip-services-oss-node");
 const SmsSettingsServiceFactory_1 = require("../build/SmsSettingsServiceFactory");
 class SmsSettingsProcess extends pip_services_container_node_1.ProcessContainer {
     constructor() {
         super("sms_settings", "Sms settings microservice");
         this._factories.add(new SmsSettingsServiceFactory_1.SmsSettingsServiceFactory);
+        this._factories.add(new pip_services_net_node_1.DefaultNetFactory);
+        this._factories.add(new pip_services_oss_node_1.DefaultOssFactory);
     }
 }
 exports.SmsSettingsProcess = SmsSettingsProcess;
--- obj/src/services/version1/SmsSettingsHttpServiceV1.js
+++ obj/src/services/version1/SmsSettingsHttpServiceV1.js
@@ -4,7 +4,7 @@ const pip_services_commons_node_1 = require("pip-services-commons-node");
 const pip_services_net_node_1 = require("pip-services-net-node");
 class SmsSettingsHttpServiceV1 extends pip_services_net_node_1.CommandableHttpService {
     constructor() {
-        super('sms_settings');
+        super('v1/sms_settings');
         this._dependencyResolver.put('controller', new pip_services_commons_node_1.Descriptor('pip-services-smssettings', 'controller', 'default', '*', '1.0'));
     }
 }

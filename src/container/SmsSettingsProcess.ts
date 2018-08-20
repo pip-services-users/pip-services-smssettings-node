30f9a2bd6fda4dc72d383784c2019935e282568f
Dmitriy Krayniy <krdima92@gmail.com>
2018-08-20 13:56:57 +0300

    added DefaultRpcFactory

--- src/container/SmsSettingsProcess.ts
+++ src/container/SmsSettingsProcess.ts
@@ -4,17 +4,16 @@ import { ProcessContainer } from 'pip-services-container-node';
 import { ActivitiesClientFactory } from 'pip-clients-activities-node';
 import { MessageTemplatesClientFactory } from 'pip-clients-msgtemplates-node';
 import { SmsClientFactory } from 'pip-clients-sms-node';
-
-import { SmsSettingsServiceFactory } from '../build/SmsSettingsServiceFactory';
+import { DefaultRpcFactory } from 'pip-services-rpc-node';
 
 export class SmsSettingsProcess extends ProcessContainer {
 
     public constructor() {
         super("sms_settings", "Sms settings microservice");
-        this._factories.add(new SmsSettingsServiceFactory);
         this._factories.add(new ActivitiesClientFactory());
         this._factories.add(new MessageTemplatesClientFactory());
         this._factories.add(new SmsClientFactory());
+        this._factories.add(new DefaultRpcFactory());
     }
 
 }
